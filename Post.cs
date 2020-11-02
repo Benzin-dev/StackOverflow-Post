cb6a27f2167236d4f17b43f2bb866da8f80475aa
Benz1n <benzinych@gmail.com>
2020-11-02 15:15:52 +0300

    Добавьте файлы проекта.

---
+++ Post.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace StackOverflow_Post
+{
+    public class Post
+    {
+        // properties:  свойства класса:
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Content { get; set; }
+        public DateTime Created { get; private set; } // date can be set only inside and can't be changed. Дата не может быть изменена извне.
+        public int Votes { get; private set; } // same
+
+        // constructor: конструктор:
+        public Post(string title, string description, string content)
+        {
+            Title = title;
+            Description = description;
+            Content = content;
+            Created = DateTime.Now;
+            Votes = 0;
+        }
+
+        // methods: методы:
+        public void VoteUp()
+        {
+            Votes++;
+        }
+
+        public void VoteDown()
+        {
+            Votes--;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+namespace StackOverflow_Post
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Create a new post");
+            Console.WriteLine("Enter Title: ");
+            var titleInput = Console.ReadLine();
+
+            Console.WriteLine("Enter Description: ");
+            var descInput = Console.ReadLine();
+
+            Console.WriteLine("Enter Post message: ");
+            var postInput = Console.ReadLine();
+
+            var post = new Post(titleInput, descInput, postInput);
+
+            Console.WriteLine("Your post created!");
+            Console.WriteLine("Type 'Up' to up-vote\n 'Down' to down-vote\n 'Show' to see your post and it's stats\n 'Quit' to exit the program");
+            
+
+            while (true)
+            {
+                var input = Console.ReadLine();
+                if (input=="Show")
+                {
+                    ShowPost(post);
+                    continue;
+                }
+                else if(input=="Up")
+                {
+                    post.VoteUp();
+                    Console.WriteLine("You voted up.\n Current rating: " + post.Votes);
+                }
+                else if (input=="Down")
+                {
+                    post.VoteDown();
+                    Console.WriteLine("You voted down.\n Current rating: " + post.Votes);
+                }
+                else if(input=="Quit")
+                {
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Please enter a valid command");
+                }
+            }
+            
+
+
+            
+        }
+
+        private static void ShowPost(Post post)
+        {
+            Console.WriteLine(@"Your post: 
+Tytle: {0}
+Description: {1}
+Content: {2}
+Created: {3}
+Votes: {4}", post.Title, post.Description, post.Content, post.Created, post.Votes);
+        }
+    }
+}
